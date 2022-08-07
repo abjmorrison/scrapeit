6606d078931b9bbfc8aae992689bbd923083d643
Alicia Morrison <35038338+abjmorrison@users.noreply.github.com>
2022-08-07 13:58:51 +0200

    removed self.pipeline

---
+++ lambda/htmlScrapeit_lambda.py
@@ -0,0 +1,14 @@
+
+#%%
+import scrapers as scrapeit
+from bih_processor import get_info
+
+def handler(event, context):
+    
+    scraper = scrapeit.scrapeHTML(event)
+    scraper = scraper.get_html()
+    links = scraper.get_links().attributes
+    values = scraper.pipeline(get_info)
+
+    return links, values
+
